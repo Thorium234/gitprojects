159936540c3c73e2c5a3d72524160de8d516e7aa
Thorium234 <inyangalapaul5@gmail.com>
2025-07-04 00:51:43 +0300

    Updated new statics

---
+++ staticfiles/cloudinary/js/jquery.cloudinary.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d3df87457deeba7a872a03416c7b36fca19f488571af5f0b9934ca90a120a5a8
+size 149395
---
+++ staticfiles/cloudinary/js/jquery.iframe-transport.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6e9864c545c938343a869fcf464beaad8cdf5fadc80a68842f47836adf225b4
+size 10891
